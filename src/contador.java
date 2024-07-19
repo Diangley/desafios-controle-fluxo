854d3976e6bf8c6d407234bfbcc768c06aa0eed8
Diangley <82831261+Diangley@users.noreply.github.com>
2024-07-19 12:57:49 -0300

    Update contador.java

--- src/contador.java
+++ src/contador.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 public class Contador {
+
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Digite o primeiro parâmetro");
-            int parametroUm = scanner.nextInt();
-            System.out.println("Digite o segundo parâmetro");
-            int parametroDois = scanner.nextInt();
+        Scanner terminal = new Scanner(System.in);
 
-                try {
-                    contar(parametroUm, parametroDois);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
 
-                } catch (RuntimeException e) {
-                    System.out.println(e.getMessage());
-                }
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.err.println(e.getMessage());
         }
     }
-    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-        if(parametroUm > parametroDois) {
-            throw new RuntimeException("O segundo parâmetro deve ser maior que o primeiro");
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
         }
 
         int contagem = parametroDois - parametroUm;
-
-        for(int i = 0; i < contagem; i++){
-            System.out.println("Imprimindo o número: " + (i+1));
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + (parametroUm + i));
         }
     }
 }
-
